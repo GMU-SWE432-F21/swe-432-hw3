7afaeaee4831ee7e3cba35c75bfb84a01e72f6df
Kevin Moran <kpmoran21@gmail.com>
2021-10-06 21:40:21 -0400

    intital commit of hw 3 starter project

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const path = require('path');
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
+
+const populations = [24019, 16489, 14128, 236842];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/cities', (req, res) => {
+  return res.json(cities)
+})
+
+app.get('/populations', (req, res) => {
+  return res.json(populations)
+})
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+module.exports = app;
