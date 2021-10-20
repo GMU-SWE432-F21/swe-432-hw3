fe1e1406c56fcb18b71c7958a2df6f730add0537
Kevin Moran <kpmoran21@gmail.com>
2021-10-20 17:24:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let serviceAccount = require(process.env.FIREBASE);
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
